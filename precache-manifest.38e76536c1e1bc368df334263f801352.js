b978a3e669ea131de5b2a78b4340a4ab8954438e
negiFSD <sunilnegi217@rediffmail.com>
2022-06-03 17:54:13 +0530

    Updates

---
+++ precache-manifest.38e76536c1e1bc368df334263f801352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dda1c754afcc16805bae7dda283f54",
+    "url": "/starWarApi/index.html"
+  },
+  {
+    "revision": "64e643faeba39c02adb8",
+    "url": "/starWarApi/static/css/main.33b3ddfc.chunk.css"
+  },
+  {
+    "revision": "ec6d2491eb27b77e3fea",
+    "url": "/starWarApi/static/js/2.c4efec7c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/starWarApi/static/js/2.c4efec7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e643faeba39c02adb8",
+    "url": "/starWarApi/static/js/main.59aaaa0b.chunk.js"
+  },
+  {
+    "revision": "16fd9f7278f0c1b1883d",
+    "url": "/starWarApi/static/js/runtime-main.20bf3dc8.js"
+  }
+]);
